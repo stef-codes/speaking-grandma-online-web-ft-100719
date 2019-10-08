39e9f18660a7e3b2a56e4487f0f1b953cf9798ee
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-08 04:10:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,10 +15,10 @@
 
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
-    return "NO, NOT SINCE 1938!"
+     "NO, NOT SINCE 1938!"
   elsif phrase != phrase.upcase
-    return "HUH?! SPEAK UP, SONNY!"
+     "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
+     "I LOVE YOU TOO PUMPKIN!"
 end 
 
