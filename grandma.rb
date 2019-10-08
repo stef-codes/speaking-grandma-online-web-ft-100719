41d7303516671397d873d1a69869603c5aaf9282
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-08 04:08:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,8 @@ def speak_to_grandma(phrase)
     return "NO, NOT SINCE 1938!"
   elsif phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == 
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    return 
   
 end 
 
